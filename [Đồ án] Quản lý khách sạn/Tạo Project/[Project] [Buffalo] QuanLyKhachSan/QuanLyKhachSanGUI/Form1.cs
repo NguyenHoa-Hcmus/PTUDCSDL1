84f7b681ef98a8d67c159850f1555002a46b4385
HoaNguyen-Hcmus <32710133+HoaNguyen-Hcmus@users.noreply.github.com>
2017-11-29 22:57:08 +0700

    hÒA CẬP NHẬT

--- [Đồ án] Quản lý khách sạn/Tạo Project/[Project] [Buffalo] QuanLyKhachSan/QuanLyKhachSanGUI/Form1.cs
+++ [Đồ án] Quản lý khách sạn/Tạo Project/[Project] [Buffalo] QuanLyKhachSan/QuanLyKhachSanGUI/Form1.cs
@@ -16,5 +16,10 @@ namespace QuanLyKhachSanGUI
         {
             InitializeComponent();
         }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
